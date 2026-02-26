6c43b4b8d2d025915ccbea72dd013be9ca853a5e
lighteko <huijunyi0212@naver.com>
2026-02-26 23:58:33 +0900

    chore: attach swagger docs

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from '@/app.module';
 import { GlobalExceptionFilter } from '@/common/errors/global-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Reppy API')
+    .setDescription('Reppy API documentation')
+    .setVersion('1.0')
+    .build();
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('docs', app, swaggerDocument);
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
